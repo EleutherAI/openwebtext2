623c0f60abdbbf111f9d9e0e9c11872103d81608
researcher2 <32028883+researcher2@users.noreply.github.com>
2020-09-03 19:58:00 +1000

    Update download_pushshift_dumps.py
    Ensure output directory is created before starting
    Add sha256 hash verification of downloads.
    Remove dependency on content-length header

--- download_pushshift_dumps.py
+++ download_pushshift_dumps.py
@@ -7,10 +7,20 @@ from logger import setup_logger
 from dateutil.relativedelta import *
 import tqdm
 import math
+import hashlib
 
 logger = logging.getLogger()
 
 million = math.pow(10, 6)
+
+def calculate_file_sha256sum(file_path):
+    sha256_hash = hashlib.sha256()
+
+    with open(file_path,"rb") as f:
+        for byte_block in iter(lambda: f.read(4096),b""):
+            sha256_hash.update(byte_block)
+
+    return sha256_hash.hexdigest()
 
 def download_file(url, local_directory):
     local_file_path = os.path.join(local_directory, url.split('/')[-1])
@@ -18,9 +28,14 @@ def download_file(url, local_directory):
 
     file_size_req = requests.get(url, stream=True)
     file_size_req.raise_for_status()
-    file_size = int(file_size_req.headers['Content-length'])
-    file_size_mb = int(file_size / million)
-    logger.info(f"File Size: {file_size_mb}MB")
+
+    file_size = None
+    if "Content-Length" in file_size_req.headers:
+        file_size = int(file_size_req.headers['Content-length'])
+        file_size_mb = int(file_size / million)
+        logger.info(f"File Size: {file_size_mb}MB")
+    else:
+        logger.info("No content length header from server. RUUUUDE!")
 
     chunk_size = 8192
     with requests.get(url, stream=True) as r, \
@@ -31,10 +46,13 @@ def download_file(url, local_directory):
         r.raise_for_status()
         with open(local_file_path, 'wb') as f:
             for chunk in r.iter_content(chunk_size): 
-                progress.update(chunk_size)
+                progress.update(len(chunk))
                 f.write(chunk)
 
 def download_pushshift_dumps(start_date, end_date, download_directory):
+    if not os.path.isdir(download_directory):
+        os.makedirs(download_directory, exist_ok=True)
+
     # First available file: RS_2011-01.bz2
     base_url = "https://files.pushshift.io/reddit/submissions/"
     possible_archive_formats = ["bz2", "xz", "zst"]
@@ -57,10 +75,27 @@ def download_pushshift_dumps(start_date, end_date, download_directory):
 
         date = date + relativedelta(months=+1)
 
+def get_sha256sums(download_directory):
+    sha256sum_url = "https://files.pushshift.io/reddit/submissions/sha256sums.txt"
+    download_file(sha256sum_url, download_directory)
+    local_path = os.path.join(download_directory, "sha256sums.txt")
+
+    sha256sum_lookup = {}
+    with open(local_path, "r") as fh:
+        for line in fh:
+            if line.strip():
+                sha256sum, file_name = tuple(line.strip().split("  "))
+                sha256sum_lookup[file_name] = sha256sum
+
+    return sha256sum_lookup
+
+
 def verify_dumps(start_date, end_date, download_directory):
 
+    sha256sum_lookup = get_sha256sums(download_directory)
     possible_archive_formats = ["bz2", "xz", "zst"]
     missing = []
+    bad_hash = []
 
     date = start_date
     while date <= end_date:
@@ -71,8 +106,15 @@ def verify_dumps(start_date, end_date, download_directory):
             file_name = f"RS_{year}-{month}.{extension}"
             file_path = os.path.join(download_directory, file_name)
             if os.path.exists(file_path):
-                success = True
-                break
+                calculated_hash = calculate_file_sha256sum(file_path)
+                if calculated_hash != sha256sum_lookup[file_name]:
+                    bad_hash.append(file_name)
+                    logger.info(f"sha256 doesn't match for file {file_path}")
+                else:
+                    logger.info(f"{file_path} hash validated")
+                    success = True
+                    break
+
         if not success:
             missing.append(date.strftime("%Y-%m"))
 
@@ -99,14 +141,15 @@ def main(logfile_path, start_date, end_date, download_directory):
 
 if __name__ == '__main__':
     logfile_path = "download_pushshift_dumps.log"
+
     # start_date = datetime.datetime(2011, 1, 1)    
     # end_date = datetime.datetime.now()
 
     # start_date = datetime.datetime(2020, 4, 1)    
     # end_date = datetime.datetime.now()
 
-    start_date = datetime.datetime(2017, 12, 1)
-    end_date = datetime.datetime(2017, 12, 1)
+    start_date = datetime.datetime(2011, 1, 1)
+    end_date = datetime.datetime(2011, 1, 1)
 
     download_directory = "E:/Eleuther_AI/webtext2/dumps"
 
